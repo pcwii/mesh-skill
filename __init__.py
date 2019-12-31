942a4e0986d737b97c28229c8d7723777d0c3aa3
philc <philc@arcx.com>
2019-12-30 20:57:21 -0500

    Major update, prior to online testing -Contains all major elements
    Change-Id: I43c2d0948ff4f6fb60b190b4350736077a0dc20a

--- __init__.py
+++ __init__.py
@@ -3,10 +3,13 @@ from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler, intent_file_handler
 from mycroft.util.log import getLogger
 from mycroft.util.log import LOG
+from mycroft.audio import wait_while_speaking
 
 from time import sleep
+import uuid
 import string
 import random
+import json
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 
@@ -27,9 +30,8 @@ class MeshSkill(MycroftSkill):
     # The constructor of the skill, which calls Mycroft Skill's constructor
     def __init__(self):
         super(MeshSkill, self).__init__(name="MeshSkill")
-        self.myKeywords = []
-        # self.client = ''  # mqtt.Client()
-
+        self.deviceUUID = ''
+        self.targetDevice = ''
         # Initialize settings values
         self.client = mqtt.Client(self.id_generator())
         self._is_setup = False
@@ -39,47 +41,76 @@ class MeshSkill(MycroftSkill):
     # creates and registers each intent that the skill uses
     def initialize(self):
         self.load_data_files(dirname(__file__))
-
         #  Check and then monitor for credential changes
         self.settings.set_changed_callback(self.on_websettings_changed)
         self.on_websettings_changed()
-
-        self.add_event('recognizer_loop:wakeword', self.handle_listen)  # should be "utterance"
-        self.add_event('recognizer_loop:utterance', self.handle_utterances) # should be "utterances"
-        self.add_event('speak', self.handle_speak)# should be "utterance"
-
+        self.deviceUUID = self.get_mac_address()
+        #self.add_event('recognizer_loop:wakeword', self.handle_listen)  # should be "utterance"
+        self.add_event('recognizer_loop:utterance', self.handle_utterances)  # should be "utterances"
+        self.add_event('speak', self.handle_speak)  # should be "utterance"
 
     def on_websettings_changed(self):  # called when updating mycroft home page
-        #if not self._is_setup:
+        self.MQTT_Enabled = self.settings.get("MQTT_Enabled", False)  # used to enable / disable mqtt
         self.broker_address = self.settings.get("broker_address", "192.168.0.43")
         self.broker_port = self.settings.get("broker_port", 1883)
-        self.plcOutTagName = self.settings.get("plc_out_tag_name", "StartRobot")
+        self.location_id = self.settings.get("location_id", "basement")
         self._is_setup = True
         LOG.info("Websettings Changed! " + self.broker_address + ", " + str(self.broker_port))
+        self.mqtt_init()
+
+    def mqtt_init(self):  #initializes the MQTT configuration
+        mqttPath = "Mycroft/RemoteDevices/" + self.location_id
+        self.client.on_message = self.on_message
+        self.client.connect(self.broker_address, self.broker_port, 60)
+        self.client.subscribe(mqttPath, 0)
+        self.client.loop_start()
+
+    def on_message(self, client, obj, msg):  # called when a new MQTT message is received
+        LOG.info('message received for location id: ' + self.location_id)
+        LOG.info(msg.payload)
+        mqtt_message = str(msg.payload)[2:-1]
+        new_message = json.loads(mqtt_message)
+        LOG.info(msg.topic + " " + str(msg.qos) + ", " + mqtt_message)
+        if "command" in new_message:
+            LOG.info('Command Received! - ' + new_message["command"])
+        elif "message" in new_message:
+            LOG.info('Message Received! - ' + new_message["message"])
+            self.speak_dialog('location.dialog', data={"location": new_message["source"]}, expect_response=False)
+            wait_while_speaking()
+            self.speak_dialog('message.dialog', data={"message": new_message["message"]}, expect_response=False)
+
 
     def id_generator(self, size=6, chars=string.ascii_uppercase + string.digits):
         return ''.join(random.choice(chars) for _ in range(size))
 
-    # utterance event used for notifications ***This is what the user says***
+    def get_mac_address():  #used to create a unique UUID for this device that
+        node = uuid.getnode()
+        mac = uuid.UUID(int=node).hex[-12:]
+        LOG.info("MQTT using UUID: " + mac)
+        return mac
+
+    # utterance event used for notifications ***This is what the user requests***
     def handle_utterances(self, message):
+        mqttPath = "Mycroft/" + self.deviceUUID + "/request"
+        LOG.info(mqttPath)
         voice_payload = str(message.data.get('utterances')[0])
         if self.notifier_bool:
             try:
                 LOG.info(voice_payload)
-                self.send_MQTT("Mycroft/Student", voice_payload)
-
+                self.send_MQTT(mqttPath, voice_payload)
             except Exception as e:
                 LOG.error(e)
                 self.on_websettings_changed()
 
     # mycroft speaking event used for notificatons ***This is what mycroft says***
     def handle_speak(self, message):
+        mqttPath = "Mycroft/" + self.deviceUUID + "/response"
+        LOG.info(mqttPath)
         voice_payload = message.data.get('utterance')
         if self.notifier_bool:
             try:
                 LOG.info(voice_payload)
-                self.send_MQTT("Mycroft/AI", voice_payload)
-                #self.card_conversation()
+                self.send_MQTT(mqttPath, voice_payload)
             except Exception as e:
                 LOG.error(e)
                 self.on_websettings_changed()
@@ -87,16 +118,53 @@ class MeshSkill(MycroftSkill):
     def send_MQTT(self, myTopic, myMessage):
         if self.MQTT_Enabled:
             LOG.info("MQTT: " + myTopic + ", " + myMessage)
-            myID = self.id_generator()
-            #LOG.info("MyID: " + str(myID))
-            #self.client = mqtt.Client(myID)
-            #self.client.connect(self.broker_address, self.broker_port)  # connect to broker
-            #self.client.publish(myTopic, myMessage)  # publish
-            #self.client.disconnect()
+            #myID = self.id_generator()
             LOG.info("address: " + self.broker_address + ", Port: " + str(self.broker_port))
             publish.single(myTopic, myMessage, hostname=self.broker_address)
         else:
             LOG.info("MQTT has been disabled in the websettings at https://home.mycroft.ai")
+
+    # First step in the dialog is to receive the initial request to "send a message/command"
+    @intent_handler(IntentBuilder("SendMessageIntent").require("SendKeyword").
+                    one_of("MessageKeyword", "CommandKeyword").build())
+    def handle_send_message_intent(self, message):
+        self.set_context('GetLocationContextKeyword', 'GetLocationContext')
+        if "MessageKeyword" in message.data:
+            self.set_context('MessageKeyword', 'message')
+        if "CommandKeyword" in message.data:
+            self.set_context('CommandKeyword', 'command')
+        self.speak_dialog('request.location.dialog', data={"result": message.data}, expect_response=True)
+
+    # Second step in the dialog is to request the location to send the message/command
+    @intent_handler(IntentBuilder("GetLocationIntent").require("GetLocationContextKeyword").
+                    one_of("MessageKeyword", "CommandKeyword").build())
+    def handle_get_location_intent(self, message):
+        self.set_context('GetLocationContextKeyword', '')
+        self.set_context('GetDetailsContextKeyword', 'GetDetailsKeyword')
+        self.targetDevice = str(message.utterance_remainder())
+        if "MessageKeyword" in message.data:
+            self.set_context('MessageKeyword', 'message')
+        if "CommandKeyword" in message.data:
+            self.set_context('CommandKeyword', 'command')
+        self.speak_dialog('request.details.dialog', data={"result": message.data}, expect_response=True)
+
+    # Third step is to combine everything
+    @intent_handler(IntentBuilder("GetDetailsIntent").require("GetDetailsContextKeyword").
+                    one_of("MessageKeyword", "CommandKeyword").build())
+    def handle_get_details_intent(self, message):
+        message_json = {}  # create json object
+        self.set_context('GetLocationContextKeyword', '')
+        self.set_context('GetDetailsContextKeyword', '')
+        message_json['source'] = self.location_id
+        if "MessageKeyword" in message.data:
+            LOG.info("Preparing to Send a message to " + self.targetDevice)
+            message_json['message'] = str(message.utterance_remainder())
+        if "CommandKeyword" in message.data:
+            LOG.info("Preparing to Send a command to " + self.targetDevice)
+            message_json['command'] = str(message.utterance_remainder())
+        LOG.info("Sending the following : " + str(message_json))
+        mqtt_path = "Mycroft/RemoteDevices/" + self.targetDevice
+        self.send_MQTT(mqtt_path, message_json)
 
     def stop(self):
         pass
