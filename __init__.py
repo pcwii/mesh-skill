2fa000dfdbabba40b8139d9985f2603667be3046
philc <philc@arcx.com>
2020-01-02 16:35:35 -0500

    change mqtt subscription loop
    Change-Id: I0c1b008d6ff8d87f5c7d3385cc7cdb66f6e8cb2c

--- __init__.py
+++ __init__.py
@@ -74,7 +74,8 @@ class MeshSkill(MycroftSkill):
             self.client.connect(self.broker_address, self.broker_port, 60)
             self.client.subscribe(mqtt_path, 0)
             LOG.info('Mesh-Skill Subscribing to: ' + mqtt_path)
-            self.client.loop_start()
+            #self.client.loop_start()
+            self.client.loop_forever()
         # else:
             #LOG.info('MQTT Not Enabled')
             #self.on_websettings_changed()
