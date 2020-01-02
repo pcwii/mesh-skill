7742727ab5f086a424cc5ea9117bc45b625b1a09
philc <philc@arcx.com>
2020-01-02 16:53:18 -0500

    updated Quality of Service
    Change-Id: I53b69fd72c44d574dad14a1e8092fa0049259862

--- __init__.py
+++ __init__.py
@@ -72,8 +72,8 @@ class MeshSkill(MycroftSkill):
             mqtt_path = self.base_topic + "/RemoteDevices/" + self.location_id
             self.client.on_message = self.on_message
             self.client.connect(self.broker_address, self.broker_port, 60)
-            QOS = 0
-            self.client.subscribe(mqtt_path, QOS)
+            qos = 0
+            self.client.subscribe(mqtt_path, qos)
             LOG.info('Mesh-Skill Subscribing to: ' + mqtt_path)
             while True:
                 self.client.loop_start()
